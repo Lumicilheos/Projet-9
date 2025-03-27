19b0f5856a7326a2fc50ff2a2709e21a7e43ec00
Louis-Michel HALBWAX <l.halbwax@orange.fr>
2025-03-27 18:35:02 +0100

    key slider+style camel case+ordre slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,12 +7,11 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
+  const byDateDesc = data?.focus.sort((evtA, evtB) => (new Date(evtA.date) < new Date(evtB.date) ? -1 : 1));
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      // () => setIndex(index < byDateDesc.length ? index + 1 : 0), TEST POUR RETOURNER A 0
+      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
       5000
     );
   };
@@ -22,13 +21,9 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
+        // key mis dans une div a part
+        <div key={event.title}>
+          <div className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
@@ -41,16 +36,11 @@ const Slider = () => {
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
+                <input key={`${event.id}`} type="radio" name="radio-button" checked={idx === radioIdx} />
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
